d400cb2dc262152853c33694bd6536b6e53c7bb6
pranav01 <pranavreddy89@yahoo.in>
2015-10-16 19:20:00 +0000

    Initial Push

---
+++ README
@@ -0,0 +1,7 @@
+export PATH=~/Your_Toolchain_PATH/
+for example /alps/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.6/bin
+Build Command:
+kernel
+======
+cd kernel directory
+TARGET_PRODUCT=wiko MTK_ROOT_CUSTOM=../mediatek/custom/ MTK_PATH_SOURCE=../mediatek/kernel/ MTK_PATH_PLATFORM=../mediatek/platform/mt6572/kernel/ make
---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Thu Aug 22 13:59:58 CST 2013"
+#define LINUX_COMPILE_BY "ubuntu"
+#define LINUX_COMPILE_HOST "HT139-UBUNTU-64"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
